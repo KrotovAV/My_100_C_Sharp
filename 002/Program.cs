d5c54c1651cea9d422ffa79f13c5ed1728073f85
KrotovAV <KrotovAV@tut.by>
2022-09-09 07:11:34 +0300

    002

---
+++ 002/Program.cs
@@ -0,0 +1,21 @@
+﻿//  2. Даны два числа. Показать большее и меньшее число
+
+Console.WriteLine("");
+
+Console.WriteLine("Write namber a");
+double a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write namber b");
+double b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("-------");
+
+if(a < b)
+{
+    Console.WriteLine($"Namber a ({a}) is min, namber b ({b}) is max");
+}
+else
+{
+    Console.WriteLine($"Namber a ({a}) is max, namber b ({b}) is min");
+}
+Console.WriteLine("");
