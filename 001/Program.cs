784a24cca710270c56f3719519f04225ca11ec4d
KrotovAV <KrotovAV@tut.by>
2022-09-09 07:01:29 +0300

    001

---
+++ 001/Program.cs
@@ -0,0 +1,21 @@
+﻿// 1. По двум заданным числам проверять является ли первое квадратом второго
+
+Console.WriteLine("");
+
+Console.WriteLine("Write namber a");
+double a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write power namber");
+double b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("-------");
+
+if(b*b ==a)
+{
+    Console.WriteLine("Namber a is a power b");
+}
+else
+{
+    Console.WriteLine("Namber a is not a power b");
+}
+Console.WriteLine("");
