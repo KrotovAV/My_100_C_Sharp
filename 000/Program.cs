5b5bebebfca08ae97f5860a4c494c93957be4b88
KrotovAV <KrotovAV@tut.by>
2022-09-09 07:00:49 +0300

    000

---
+++ 000/Program.cs
@@ -0,0 +1,13 @@
+﻿//  0. Вывести квадрат числа
+
+Console.WriteLine("");
+
+Console.WriteLine("Write namber a");
+double a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("-------");
+double b = a * a;
+Console.WriteLine(b);
+
+Console.WriteLine("");
+
