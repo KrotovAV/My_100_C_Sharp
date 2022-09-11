0d354cae19ea801b78b91a433ff01fb00a2f4f96
KrotovAV <KrotovAV@tut.by>
2022-09-11 09:24:33 +0300

    007

---
+++ 007/Program.cs
@@ -0,0 +1,12 @@
+﻿//  7. Показать числа от -N до N
+
+Console.WriteLine();
+Console.WriteLine("Write number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("------------------");
+for (int i = - a; i < a +1; i++)
+{
+    Console.Write($"{i}, ");
+}
+Console.WriteLine();
+Console.WriteLine();
