fea28adfba6b1994b3ec22c10797762455a69bb0
KrotovAV <KrotovAV@tut.by>
2022-09-11 09:57:51 +0300

    010

---
+++ 010/Program.cs
@@ -0,0 +1,17 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+
+Console.WriteLine();
+Console.WriteLine("Write three-digit number");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("------------------");
+
+if(a > 999 || a < 99 ) 
+{
+    Console.WriteLine("Write namber is not a three-digit number"); 
+    Console.WriteLine();
+    return;
+}
+
+Console.WriteLine((a % 100- a % 10)/10);
+Console.WriteLine();
