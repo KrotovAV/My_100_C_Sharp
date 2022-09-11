b8c4e6344d34346a101ffdc175000514d2ba2e99
KrotovAV <KrotovAV@tut.by>
2022-09-11 10:31:06 +0300

    012

---
+++ 012/Program.cs
@@ -0,0 +1,18 @@
+﻿// 12. Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine();
+Console.WriteLine("Write three-digit number");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("------------------");
+
+if(a > 999 || a < 99 ) 
+{
+    Console.WriteLine("Write namber is not a three-digit number"); 
+    Console.WriteLine();
+    return;
+}
+
+Console.WriteLine($"New two-digit namber is {(a / 100)*10 + a % 10}. "); 
+//Console.WriteLine((a / 100)*10 + a % 10);
+Console.WriteLine();
