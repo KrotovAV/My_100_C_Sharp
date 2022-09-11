8a0bdf21152c5d510182b2b21e8d32179253d858
KrotovAV <KrotovAV@tut.by>
2022-09-11 10:10:58 +0300

    011

---
+++ 011/Program.cs
@@ -0,0 +1,32 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+
+Console.WriteLine();
+Console.WriteLine("Write two-digit number");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("------------------");
+
+if(a > 99 || a < 9 ) 
+{
+    Console.WriteLine($"Namber {a} is not a two-digit number"); 
+    Console.WriteLine();
+    return;
+}
+
+Console.Write("Bigest namber is "); 
+
+if((a % 100- a % 10)/10>a % 10)
+{
+    Console.WriteLine((a % 100- a % 10)/10);
+}
+else{
+    Console.WriteLine(a % 10);
+}
+//int b = (a % 100- a % 10)/10;
+//int c = a % 10;
+
+
+//Console.WriteLine(b);
+//Console.WriteLine(c);
+Console.WriteLine();
