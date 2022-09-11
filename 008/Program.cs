d0af3c692d34c9add874fcddb0b2839a2febf1d5
KrotovAV <KrotovAV@tut.by>
2022-09-11 09:35:22 +0300

    008

---
+++ 008/Program.cs
@@ -0,0 +1,13 @@
+﻿// 8. Показать четные числа от 1 до N
+
+Console.WriteLine();
+Console.WriteLine("Write number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("------------------");
+
+for (int i = 2; i < a + 1; i+=2)
+{
+    Console.Write($"{i}, ");
+}
+Console.WriteLine();
+Console.WriteLine();
