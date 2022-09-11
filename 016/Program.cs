e651427cd2c8401b945a055c647b6589255c9211
KrotovAV <KrotovAV@tut.by>
2022-09-11 11:58:53 +0300

    016

---
+++ 016/Program.cs
@@ -0,0 +1,25 @@
+﻿// 16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
+
+Console.WriteLine();
+
+Console.WriteLine("Write a number from 1 to 7");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("-------");
+if(a <1 || a > 7) 
+{
+    Console.WriteLine("!!! Number is not a number from 1 to 7 from 1 to 7"); 
+    Console.WriteLine(""); 
+    return;
+}
+
+if(a > 5)
+{
+    Console.WriteLine("The day is day off."); 
+}
+else
+{
+    Console.WriteLine("The day is working day."); 
+}
+
+Console.WriteLine("");
