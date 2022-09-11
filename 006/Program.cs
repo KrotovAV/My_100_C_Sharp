578584270b30c043c249ee7672ea33e24827da24
KrotovAV <KrotovAV@tut.by>
2022-09-11 09:11:10 +0300

    006

---
+++ 006/Program.cs
@@ -0,0 +1,17 @@
+﻿//  6. Выяснить является ли число чётным
+
+Console.WriteLine();
+Console.WriteLine("Write number");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("------------------");
+if(a % 2 == 0)
+{
+Console.WriteLine($"Number {a} is even.");
+}
+else
+{
+    Console.WriteLine($"Number {a} is odd.");
+}
+//Console.WriteLine(a % 2);
+Console.WriteLine();
