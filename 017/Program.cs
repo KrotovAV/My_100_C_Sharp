75af37302bcbe82b7f2124c4e47df4466660d4ba
KrotovAV <KrotovAV@tut.by>
2022-09-11 12:03:44 +0300

    017

---
+++ 017/Program.cs
@@ -0,0 +1,21 @@
+﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("");
+
+Console.WriteLine("Write namber a");
+double a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write power namber");
+double b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("-------");
+
+if(b*b == a)
+{
+    Console.WriteLine("Namber a is a power b");
+}
+else
+{
+    Console.WriteLine("Namber a is not a power b");
+}
+Console.WriteLine("");
