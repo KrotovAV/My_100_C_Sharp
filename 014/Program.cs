83a0c0c0d5455be494de738d71199cd2d54eaccb
KrotovAV <KrotovAV@tut.by>
2022-09-11 11:34:36 +0300

    014

---
+++ 014/Program.cs
@@ -0,0 +1,24 @@
+﻿// 14. Найти третью цифру числа или сообщить, что её нет
+
+
+Console.WriteLine();
+Console.WriteLine("Write a number");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("------------------");
+
+if(a < 99 ) 
+{
+    Console.WriteLine("Third-digit is not exist in namber."); 
+    Console.WriteLine();
+    return;
+}
+
+do
+{
+    a = a/10;
+} while (a > 999);
+
+Console.WriteLine(a % 10);
+
+Console.WriteLine();
