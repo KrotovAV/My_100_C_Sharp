1b22c45b73c71984209e4c869c0be4693203445f
KrotovAV <KrotovAV@tut.by>
2022-09-11 08:25:41 +0300

    004

---
+++ 004/Program.cs
@@ -0,0 +1,20 @@
+﻿//  4. Найти максимальное из трех чисел
+
+Console.WriteLine();
+
+Console.WriteLine("Write first number");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write second number");
+int b = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Write third number");
+int c = int.Parse(Console.ReadLine());
+
+Console.WriteLine("-----------------");
+
+if(a > b && a > c) Console.WriteLine($"Number {a} is the biggest");
+if(b > a && b > c) Console.WriteLine($"Number {b} is the biggest");
+if(c > a && c > b) Console.WriteLine($"Number {c} is the biggest");
+Console.WriteLine("");
