003aa96d18adb2334e2de64c68723cd572978738
KrotovAV <KrotovAV@tut.by>
2022-09-11 08:58:03 +0300

    005

---
+++ 005/Program.cs
@@ -0,0 +1,20 @@
+﻿// 5. Написать программу вычисления значения функции y = f(a)
+
+Console.WriteLine();
+
+Console.WriteLine("Here is the function: y = a * x + b");
+Console.WriteLine();
+Console.WriteLine("Write number a");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write number b");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write number x");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("------------------");
+int y = a * x + b;
+Console.WriteLine($"y = {y}");
+Console.WriteLine();
+
