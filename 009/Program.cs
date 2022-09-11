f0af376e5c30259eaa768c533dd611f51f2e2a49
KrotovAV <KrotovAV@tut.by>
2022-09-11 09:48:26 +0300

    009

---
+++ 009/Program.cs
@@ -0,0 +1,19 @@
+﻿// 9. Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine();
+Console.WriteLine("Write three-digit number");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("------------------");
+
+if(a > 999 || a < 99 ) 
+{
+    Console.WriteLine("Write namber is not a three-digit number"); 
+    Console.WriteLine();
+    return;
+}
+
+int b = a % 10;
+
+Console.WriteLine(b);
+Console.WriteLine();
